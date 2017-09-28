f9edb5d8b24cc8a0994d192a3563f30d1c47dbfb
Edison Alday <edison.alday@gotouche.com>
2017-09-28 17:51:37 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Create the hellowhale service deployment"
-kubectl apply -f hellowhale_deployment.yaml 
-kubectl set image deployment/hellowhale hellowhale=$IMAGE_NAME
-kubectl apply -f hellowhale_pod.yaml 
+
+kubectl apply -f hellowhale_deployment.yaml --IMAGE_NAME="ealday/hellowhale:${BUILD_NUMBER}"
+kubectl apply -f hellowhale_pod.yaml --IMAGE_NAME="ealday/hellowhale:${BUILD_NUMBER}"
 kubectl apply -f hellowhale_service.yaml
