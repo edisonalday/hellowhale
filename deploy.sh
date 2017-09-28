6ef8337baa280f2c4632fe11389ffe61b2072834
Edison Alday <edison.alday@gotouche.com>
2017-09-28 17:04:04 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Create the hellowhale service deployment"
-kubectl create -f hellowhale_pod.yaml 
-kubectl create -f hellowhale_service.yaml
+kubectl set -f hellowhale_pod.yaml 
+kubectl set -f hellowhale_service.yaml
