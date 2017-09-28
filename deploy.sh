2171ad417bbc52cf570ba71d96acc036ad07b18a
Edison Alday <edison.alday@gotouche.com>
2017-09-28 16:58:01 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Create the hellowhale service deployment"
-kubectl create -f hellowhale_pod.yaml --token=${KUBERNETES_DEF_AUTH_TOKEN} --namespace=${KUBERNETES_NAMESPACE}
-kubectl create -f hellowhale_service.yaml --token=${KUBERNETES_DEF_AUTH_TOKEN} --namespace=${KUBERNETES_NAMESPACE}
+kubectl create -f hellowhale_pod.yaml 
+kubectl create -f hellowhale_service.yaml
