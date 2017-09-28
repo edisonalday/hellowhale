2700c5b655e6412a134fd69447b382c79afdfc4d
Edison Alday <edison.alday@gotouche.com>
2017-09-28 16:46:21 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 echo "Create the hellowhale service deployment"
 kubectl config --server=${KUBERNETES_SERVER} --insecure-skip-tls-verify=true
-kubectl apply -f hellowhale_pod.yaml --token=${KUBERNETES_DEF_AUTH_TOKEN} --namespace=default
-kubectl apply -f hellowhale_service.yaml --token=${KUBERNETES_DEF_AUTH_TOKEN} --namespace=default
+kubectl create -f hellowhale_pod.yaml --token=${KUBERNETES_DEF_AUTH_TOKEN} --namespace=default
+kubectl create -f hellowhale_service.yaml --token=${KUBERNETES_DEF_AUTH_TOKEN} --namespace=default
